563b230c7365dbf07d351f86596ee47ff086b648
jennydunix <jennifer.o.odunze@gmail.com>
2023-11-25 12:33:55 +0100

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+// initialize dotenv
+dotenv.config();
+
+// to connect to MongoDB
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
